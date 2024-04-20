99fe271e4ef1ff061b9c3296e6b7ab742e6151d0
ryanyychen <ryan_yychen@hotmail.com>
2024-04-20 14:54:07 -0700

    Setup Kmean

--- lahacks2024/create_lobby.py
+++ lahacks2024/create_lobby.py
@@ -2,47 +2,89 @@ import reflex as rx
 import requests
 import googlemaps
 from dotenv import load_dotenv
+from lahacks2024.cluster import Kmeans
 import os
 
 # Keep track of entered users
 class State(rx.State):
-    users: dict = {}
+    users: list = [{},{}]
 
     # Add user to dictionary
     def add_user(self, form_data):
         name = form_data["Name"]
         location = form_data["Location"]
-        self.users[name] = location
-        self.get_geocoding_all()
+        if (form_data["User Type"] == "Driver"):
+            self.users[0][name] = location
+        else:
+            self.users[1][name] = location
+    
+    def calculate(self):
+        coords = self.get_geocoding_all()
+
+        x = []
+        y = []
+        centroids = []
+        for coord in coords[0]:
+            x.append(coord[0])
+            y.append(coord[1])
+            centroids.append(coord)
+        for coord in coords[1]:
+            x.append(coord[0])
+            y.append(coord[1])
+        clusters = Kmeans.cluster([x,y], centroids)
+        print(clusters)
+    
+    def user_list_type(self, type):
+        users_l = []
+        for each in self.users[type]:
+            users_l.append(each)
+        return users_l
     
     # Return names of users added
     def user_list(self):
         users_l = []
-        for each in self.users:
-            users_l.append(each)
+        for type in self.users:
+            for each in type:
+                print(each)
+                users_l.append(each)
         return users_l
     
-    # Return locations of users added
-    def loc_list(self):
+    # Return locations of drivers added
+    def loc_list_drivers(self):
         loc_l = []
-        for each in self.users:
-            loc_l.append(self.users[each])
+        for each in self.users[0]:
+            loc_l.append(self.users[0][each])
+        return loc_l
+    
+    # Return locations of passengers added
+    def loc_list_passengers(self):
+        loc_l = []
+        for each in self.users[1]:
+            loc_l.append(self.users[1][each])
         return loc_l
 
     # Return latitude, longitude coordinates of each address
     def get_geocoding_all(self):
-        locations = self.loc_list()
-        geocodes = []
+        driver_locations = self.loc_list_drivers()
+        passenger_locations = self.loc_list_passengers()
+        driver_geocodes = []
+        passenger_geocodes = []
 
-        for location in locations:
+        for location in driver_locations:
             geocode = get_geocoding(location)
-            geocodes.append(geocode)
+            driver_geocodes.append([geocode["longitude"], geocode["latitude"]])
         
-        for code in geocodes:
+        for location in passenger_locations:
+            geocode = get_geocoding(location)
+            passenger_geocodes.append([geocode["longitude"], geocode["latitude"]])
+
+        for code in driver_geocodes:
             print(code)
         
-    def change_user_type(self):
-        pass
+        for code in passenger_geocodes:
+            print(code)
+
+        return [driver_geocodes, passenger_geocodes]
 
 def get_geocoding(address):
     """
@@ -59,7 +101,6 @@ def get_geocoding(address):
     load_dotenv('lahacks2024/.env')
     
     api_key = os.getenv('GOOGLE_API')  # Replace with your actual API Key
-    print(api_key)
     base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
     params = {
         "address": address,
@@ -117,9 +158,10 @@ def get_distance_matrix(api_key, origins, destinations, travel_mode="driving"):
     return None
 
 def display_user_list():
-        return rx.vstack(
-            rx.foreach(State.users, rx.text),
-        )
+    driver_list: list = State.user_list_type(0)
+    print(State.user_list_type(0))
+    print(State.users[0].items())
+    return rx.vstack()
 
 def create_lobby():
     return rx.vstack(
@@ -133,7 +175,7 @@ def create_lobby():
                 direction="row",
                 spacing="3",
                 size="3",
-                # on_change=State.change_user_type,
+                name="User Type",
                 required=True,
             ),
             rx.button(
@@ -141,6 +183,10 @@ def create_lobby():
                 type="submit",
             ),
             on_submit=State.add_user,
+        ),
+        rx.button(
+            "Calculate",
+            on_click=State.calculate(),
         ),
         display_user_list(),
     )
